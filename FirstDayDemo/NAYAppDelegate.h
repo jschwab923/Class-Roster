633e46ef71bb61f47d381e2e9687e720f8f557dd
Jeff Schwab <JeffSchwab@Jeffs-MacBook-Pro.local>
2014-01-13 11:42:06 -0800

    Initial Commit

---
+++ FirstDayDemo/NAYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NAYAppDelegate.h
+//  FirstDayDemo
+//
+//  Created by Jeff Schwab on 1/13/14.
+//  Copyright (c) 2014 Jeff Schwab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NAYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
